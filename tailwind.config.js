8a622d86cc349bff62ec0389b50fc5b15a9d1a2c
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-01-02 18:43:27 +0545

    Configure Tailwind CSS with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
+      colors: {
+        primary: "#ffcf3a",
+        secondary: "#0063ff",
+      },
     },
   },
   plugins: [],
