5dbb40b5d7f9752fd7a9261be3d605c098e4bd79
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-01-02 18:44:13 +0545

    Configured Tailwind CSS with  container settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ export default {
         primary: "#ffcf3a",
         secondary: "#0063ff",
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
     },
   },
   plugins: [],
