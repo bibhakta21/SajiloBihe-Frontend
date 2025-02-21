475f5caaf439fe957056026a579304d09330c060
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:00:31 +0545

    Added basic Navbar component structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md p-4">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">Navbar</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
