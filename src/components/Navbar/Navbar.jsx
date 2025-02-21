b637e17e1b7680bff28ecf58066445c26ce0749e
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:09:10 +0545

    Implement mobile menu toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,29 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 
 const Navbar = () => {
   const { user } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
+
 
   const handleLogout = () => {
     logout();
     navigate("/login");
   };
   
+  <button onClick={() => setIsOpen(!isOpen)}>
+  <MdMenu size={28} />
+</button>
 
   return (
+    
     <nav className="bg-white shadow-md p-4">
       <div className="container mx-auto flex justify-between">
         <h1 className="text-2xl font-bold">Navbar</h1>
+        <div className={`lg:flex ${isOpen ? "block" : "hidden"}`}>
         <ul className="flex space-x-4">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/product">Products</Link></li>
@@ -22,6 +31,7 @@ const Navbar = () => {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
+        </div>
         <div>
           {user ? (
             <p>Welcome, {user.username}</p>
