96e0a14cb71581b56f076acea1184443841d246e
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:20:22 +0545

    Add brand information section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-
 const Footer = () => {
   return (
     <div className="bg-gray-900 text-white p-6">
-      <p className="text-center">Footer Section</p>
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Sajilo<span className="text-blue-500">Bihe</span></h1>
+        <p className="mt-2 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+          accusamus nulla labore cumque.
+        </p>
+      </div>
     </div>
   );
 };
-
-export default Footer;
