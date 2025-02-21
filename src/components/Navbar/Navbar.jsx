89290e42c393d3a22cc03d7e624689c0618f9018
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:03:57 +0545

    Setup user authentication context

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
 const Navbar = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <nav className="bg-white shadow-md p-4">
       <div className="container mx-auto flex justify-between">
@@ -13,6 +16,16 @@ const Navbar = () => {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
+        <div>
+          {user ? (
+            <p>Welcome, {user.username}</p>
+          ) : (
+            <>
+              <Link to="/login" className="px-4">Sign in</Link>
+              <Link to="/register" className="bg-blue-600 text-white px-4 py-2 rounded">Register</Link>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
