86ff57992d13c2af874cb17ca51aeb55aaab8378
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:19:18 +0545

    Created the Footer component structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-900 text-white p-6">
+      <p className="text-center">Footer Section</p>
+    </div>
+  );
+};
+
+export default Footer;
