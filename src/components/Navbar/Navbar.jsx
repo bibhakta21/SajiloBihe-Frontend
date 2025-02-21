3b786b773e561fe71bcdadc1b6ff07c58f90d5a1
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:02:35 +0545

    Integrated React Router for navigation links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md p-4">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between">
         <h1 className="text-2xl font-bold">Navbar</h1>
+        <ul className="flex space-x-4">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/product">Products</Link></li>
+          <li><Link to="/stories">Stories</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </div>
     </nav>
   );
