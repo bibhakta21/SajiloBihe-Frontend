344d76e13b7944d386dcb3f762b83ca235c8ea78
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:00:47 +0545

    added route path in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; 
+import React, { useContext } from "react";
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 
 
-import Login from "./components/Login/Login"; 
-
+import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register/Register";
+import { UserContext, UserProvider } from "./context/UserContext";
 
+const AppContent = () => {
+ 
+ 
 
-
+  return (
+    <main className="overflow-x-hidden">
+    <Toaster position="top-right" reverseOrder={false} />
+    <Navbar />
+      <Routes>
+        <Route path="/login" element={authRedirect(<Login />)} />
+        <Route path="/register" element={authRedirect(<Register />)} />
+      </Routes>
+    
+    </main>
+  );
+};
 
 const App = () => {
   return (
-    <Router>
-      <main className="overflow-x-hidden">
-     
-
-       
-        <Routes>
-      
-        
-        
-          <Route path="/login" element={<Login />} />
-        
-          <Route path="/register" element={<Register />} />
-      
-
-        </Routes>
-
-      
-      </main>
-    </Router>
+    <UserProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </UserProvider>
   );
 };
 
