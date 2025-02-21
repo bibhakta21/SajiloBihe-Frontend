117bf6e0677a33de37eff1226becb811b675e0fa
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-21 23:20:46 +0545

    Added social media icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,24 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className="bg-gray-900 text-white p-6">
       <div className="text-center">
         <h1 className="text-2xl font-bold">Sajilo<span className="text-blue-500">Bihe</span></h1>
         <p className="mt-2 text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-          accusamus nulla labore cumque.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
+        <div className="flex justify-center gap-4 mt-4">
+          <a href="#" className="text-white hover:text-blue-500">
+            <FaFacebook size={24} />
+          </a>
+          <a href="#" className="text-white hover:text-blue-500">
+            <FaInstagram size={24} />
+          </a>
+          <a href="#" className="text-white hover:text-blue-500">
+            <FaLinkedin size={24} />
+          </a>
+        </div>
       </div>
     </div>
   );
