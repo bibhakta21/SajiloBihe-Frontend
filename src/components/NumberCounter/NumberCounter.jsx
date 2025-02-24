d99b73a0e8aa9969ccf4995126493257e921a599
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-24 23:22:25 +0545

    added number counter for homepage

---
+++ src/components/NumberCounter/NumberCounter.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import CountUp from "react-countup";
+
+const NumberCounter = () => {
+  return (
+    <div className="bg-secondary text-white py-12">
+      <div className="container grid grid-cols-2 md:grid-cols-4 gap-8">
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-3xl font-semibold">
+            <CountUp
+              start={0}
+              end={150}
+              duration={3}
+              enableScrollSpy={true}
+              scrollSpyOnce={true}
+            />
+          </p>
+          <p>Venues Booked</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-3xl font-semibold">
+            <CountUp
+              end={50000}
+              separator=","
+              suffix="+"
+              duration={3}
+              enableScrollSpy={true}
+              scrollSpyOnce={true}
+            />
+          </p>
+          <p>Guests Hosted</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-3xl font-semibold">
+            <CountUp
+              end={200}
+              duration={3}
+              enableScrollSpy={true}
+              scrollSpyOnce={true}
+            />
+          </p>
+          <p>Event Types Offered</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-3xl font-semibold">
+            <CountUp
+              end={100000}
+              separator=","
+              suffix="+"
+              duration={3}
+              enableScrollSpy={true}
+              scrollSpyOnce={true}
+            />
+          </p>
+          <p>Clients Served</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NumberCounter;
