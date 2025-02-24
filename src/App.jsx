7a7263dbc8ad2e6995a12d1b897f06d1af817c0f
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-02-24 23:19:18 +0545

    Added Hero, NumberCounter, About, WhyChooseUs, Testimonial, and Contact components to homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Toaster } from "react-hot-toast";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
+import { Navigate, Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
+import Contact from "./components/contact/Contact";
 import Footer from "./components/Footer/Footer";
+import About from "./components/Hero/About";
+import Hero from "./components/Hero/Hero";
 import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
+import NumberCounter from "./components/NumberCounter/NumberCounter";
 import Register from "./components/Register/Register";
+import Testimonial from "./components/Testimonial/Testimonial";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import { UserContext, UserProvider } from "./context/UserContext";
 
 
 const AppContent = () => {
+  const { user } = useContext(UserContext);
+  const location = useLocation();
  
+
  
+  const authRedirect = (element) => {
+    return user ? <Navigate to="/" replace /> : element;
+  };
 
   return (
     <main className="overflow-x-hidden">
     <Toaster position="top-right" reverseOrder={false} />
-    <Navbar />
+
+      <Navbar />
+
       <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<><Hero /><NumberCounter /><About /><WhyChooseUs /><Testimonial /><Contact /></>} />
+        {/* Authentication Routes (Redirect if user is logged in) */}
         <Route path="/login" element={authRedirect(<Login />)} />
         <Route path="/register" element={authRedirect(<Register />)} />
+        {/* Access Denied Route */}
       </Routes>
-     <Footer />
+
+    <Footer />
     </main>
   );
 };
